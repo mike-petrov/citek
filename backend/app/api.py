594c98712bffb2a4d0bdb37c6d65378b0cde6842
Леонид <romanychev-l@users.noreply.github.com>
2019-10-23 20:31:06 +0300

    add treatment of likes\dislikes on backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -9,14 +9,34 @@ db = cluster['test']
 collection = db['projects']
 
 
-@app.route('/', methods=['POST'])
+@app.route('/cards', methods=['POST'])
 def post():
 	#x = request.json
 	results = collection.find({})
 	ans = []
 	for result in results:
+		t = result['_id']
 		del result['_id']
+		result['id'] = int(t)
 		ans.append(result)
-	print("hiiii", results)
-	print(json.dumps(ans))
+	return json.dumps(ans)
+
+@app.route('/card/<cardId>/like/<flag>', methods=['POST'])
+def update(cardId, flag):
+	if flag:
+		p = collection.find_one({'_id': cardId})
+		print(cardId)
+		like = p['countLikes']
+		collection.update_one({'_id': cardId}, {'$set':{ 'countLikes': like + 1}})
+	else:
+		p = collection.find_one({'_id': cardId})
+		like = p['countLikes']
+		collection.update_one({'_id': cardId}, {'$set':{ 'countDislikes': like - 1}})
+
+	results = collection.find({})
+	for result in results:
+		t = result['_id']
+		del result['_id']
+		result['id'] = int(t)
+		ans.append(result)
 	return json.dumps(ans)
