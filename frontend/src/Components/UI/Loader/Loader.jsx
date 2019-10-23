4aa7c1853f2c0ba8dcf6615d92245241b6b7a051
mike-petrov <mis5116@yandex.ru>
2019-10-23 13:13:37 +0300

    add Loader
    Co-Authored-By: Леонид <leonid20.0831@gmail.com>

---
+++ frontend/src/Components/UI/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+    return (
+	<div className="loader">
+		<img src="/img/loader.png" alt="" />
+	</div>
+    );
+};
+
+export default Loader;
