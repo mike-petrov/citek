aaa9ba888cd7da62a35a8ba5bf7a3ebba61986cd
den4ik-kovalev <1515dk@mail.ru>
2019-10-24 17:01:34 +0300

    add information about us and employers

--- frontend/src/Containers/About.jsx
+++ frontend/src/Containers/About.jsx
@@ -1,23 +1,102 @@
 import React from 'react';
+import Nikita from '../Images/Nikita.jpg'
+import Anastasiia from '../Images/Anastasiia.jpg'
+import Leonid from '../Images/Leonid.jpg'
+import Mikhail from '../Images/Mikhail.jpg'
+import Denis from '../Images/Denis.jpg'
+import { relative } from 'path';
 // import { Link } from 'react-router-dom';
 
+import './About.css';
+
+class DeveloperInfo extends React.Component {
+	render() {
+		return(
+			<div>
+				<div className="about_info_block">
+					<img src={this.props.photo}/>
+					<div className="about_info_text">
+						{this.props.text}
+					</div>
+				</div>
+				<div className="about_name">{this.props.name}</div>
+				<div className="about_name">{this.props.lastname}</div>
+				
+			</div>
+		)
+	}
+}
 
 class About extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			// sample: [],
-		};
+		// this.state = {
+		// 	developers: [
+		// 		{name: 'Князев Никита', photo: '../Images/Nikita.jpg'},
+		// 		{name: 'Тырышкина Анастасия', photo: '../Images/Anastasiia.jpg'},
+		// 		{name: 'Романычев Леонид', photo: '../Images/Leonid.jpg'},
+		// 		{name: 'Петров Михаил', photo: '../Images/Mikhail.jpg'},
+		// 		{name: 'Ковалев Денис', photo: '../Images/Denis.jpg'}
+		// 	  ],
+		// };
 		// this.handler = this.handler.bind(this);
 	}
 
 	render() {
 		// const { sample } = this.state;
+
+		const text_n = (
+			<div>
+				<p>Студент 3 курса СПбГУ, <br></br>
+				   факультет ПМ-ПУ, <br></br>
+				   331 группа</p> 
+				<p>Роль: TeamLead & Designer</p>
+			</div>
+			)
+		const text_a = (
+			<div>
+				<p>Студент 3 курса СПбГУ, <br></br>
+					факультет ПМ-ПУ, <br></br>
+					331 группа</p> 
+				<p>Роль: Software tester</p>
+			</div>
+			)
+		const text_l = (
+			<div>
+				<p>Студент 3 курса СПбГУ, <br></br>
+					факультет ПМ-ПУ, <br></br>
+					333 группа</p> 
+				<p>Роль: Back-end</p>
+			</div>
+			)
+		const text_m = (
+			<div>
+				<p>Студент 3 курса СПбГУ, <br></br>
+					факультет ПМ-ПУ, <br></br>
+					331 группа</p> 
+				<p>Роль: Front-end & Adviser</p>
+			</div>
+			)
+		const text_d = (
+			<div>
+				<p>Студент 3 курса СПбГУ, <br></br>
+					факультет ПМ-ПУ, <br></br>
+					332 группа</p> 
+				<p>Роль: Front-end</p>
+			</div>
+			)
+
 		return (
 			<div className="content">
-				<div className="title">О нас</div>
+				<div className="title">Команда разработчиков</div>
+				<DeveloperInfo name="Князев" lastname="Никита" photo={Nikita} text={text_n} />
+				<DeveloperInfo name="Тырышкина" lastname=" Анастасия" photo={Anastasiia} text={text_a} />
+				<DeveloperInfo name="Романычев" lastname=" Леонид" photo={Leonid} text={text_l} />
+				<DeveloperInfo name="Петров" lastname=" Михаил" photo={Mikhail} text={text_m} />
+				<DeveloperInfo name="Ковалев" lastname=" Денис" photo={Denis} text={text_d} />
 			</div>
 		);
+
 	}
 }
 
--- frontend/src/Containers/Employers.jsx
+++ frontend/src/Containers/Employers.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import Sevryukov from '../Images/sevryukov.jpg'
+import Blekanov from '../Images/blekanov.jpg'
+
+import './Employers.css';
+
+class EmployerInfo extends React.Component {
+	render() {
+		return(
+			<div>
+				<div className="emp_info_block">
+					<img src={this.props.photo}/>
+					<div className="emp_info_text">
+						{this.props.text}
+					</div>
+				</div>
+				<div className="emp_name">{this.props.name}</div>
+				<div className="emp_name">{this.props.lastname}</div>
+				
+			</div>
+		)
+	}
+}
 
 
 class Employers extends React.Component {
@@ -13,9 +35,31 @@ class Employers extends React.Component {
 
 	render() {
 		// const { sample } = this.state;
+		
+		const text_s = (
+			<div>
+				<p>Старший преподаватель кафедры технологии 
+				   программирования факультета ПМ-ПУ университета СПбГУ</p>
+				<p>Контакты: http://ssevryukov.moikrug.ru/</p>
+				<p>Тел: +7-XXX-XXX-XX-XX</p>
+			</div>
+		)
+
+		const text_b = (
+			<div>
+				<p>Кандидат технических наук, доцент кафедры технологии программирования, <br></br>
+				   заведующий кафедрой технологии программирования</p>
+				<p>Контакты: http://www.apmath.spbu.ru/ru/staff/blekanov/index.html</p>
+				<p>Комн. 267</p>
+				<p>E-mail: i.blekanov@gmail.com</p>
+			</div>
+		)
+
 		return (
 			<div className="content">
 				<div className="title">Работодатели</div>
+				<EmployerInfo name="Севрюков Сергей" lastname="Юрьевич" photo={Sevryukov} text={text_s} />
+				<EmployerInfo name="Блеканов Иван" lastname="Станиславович" photo={Blekanov} text={text_b} />
 			</div>
 		);
 	}
