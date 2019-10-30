76623d914db5134b5fb8e3744addb55b2b5f8326
Леонид <romanychev-l@users.noreply.github.com>
2019-10-30 11:54:45 +0300

    add backend routes authorization and registration

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,13 +6,12 @@ import json
 
 cluster = MongoClient("mongodb+srv://Leonid:Factor_9@cluster0-e2dix.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster['test']
-collection = db['projects']
-
+projects = db['projects']
+users = db['users']
 
 @app.route('/projects', methods=['POST'])
 def post():
-	#x = request.json
-	results = collection.find({})
+	results = projects.find({})
 	ans = []
 	for result in results:
 		t = result['_id']
@@ -21,20 +20,23 @@ def post():
 		ans.append(result)
 	return json.dumps(ans)
 
-@app.route('/project/<projectId>/like/<flag>', methods=['POST'])
-def update(projectId, flag):
+@app.route('/project/like', methods=['POST'])
+def update():
+	x = request.json
+	projectId = x['id']
+	flag = x['type']
 	if flag:
-		p = collection.find_one({'_id': projectId})
+		p = projects.find_one({'_id': projectId})
 		print(projectId)
 		like = p['countLikes']
-		collection.update_one({'_id': projectId}, {'$set':{ 'countLikes': like + 1}})
+		projects.update_one({'_id': projectId}, {'$set':{ 'countLikes': like + 1}})
 	else:
-		p = collection.find_one({'_id': projectId})
+		p = projects.find_one({'_id': projectId})
 		like = p['countDislikes']
-		collection.update_one({'_id': projectId}, {'$set':{ 'countDislikes': like - 1}})
+		projects.update_one({'_id': projectId}, {'$set':{ 'countDislikes': like - 1}})
 
 	ans = []
-	results = collection.find({})
+	results = projects.find({})
 	for result in results:
 		t = result['_id']
 		del result['_id']
@@ -42,9 +44,43 @@ def update(projectId, flag):
 		ans.append(result)
 	return json.dumps(ans)
 
-@app.route('/project/<projectId>', methods=['POST'])
-def viewproject(projectId):
-	res = collection.find_one({'_id': projectId})
+@app.route('/project', methods=['POST'])
+def viewproject():
+	x = request.json
+	res = projects.find_one({'_id': x['id']})
 	t = res['_id']
 	del res['_id']
 	return json.dumps(res)
+
+@app.route('/auth', methods=['POST'])
+def authorization():
+	x = request.json
+	login = x['login']
+	password = x['password']
+
+	if projects.find({'login': login}).limit(1).size() != 1:
+		return error: 'login'
+
+	person = projects.find({'login': login})
+	if person['password'] != password:
+		return error: 'password'
+	else:
+		return json.dumps(person)
+
+@app.route('/reg', methods=['POST'])
+def registration():
+	x = request.json
+	login = x['login']
+	password = x['password']
+	name = x['name']
+	email = x['email']
+	status = x['status']
+
+	if users.find({'email': email}).limit(1).size() != 1:
+		return error: 'email'
+
+	post = {'_id': len(users),'login': login, 'password': password, 'name': name, 'email': email, 'status': status}
+
+	users.update_one(post)
+
+	return json.dumps(post)
