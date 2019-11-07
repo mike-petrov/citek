2cb79f595859be55b2fb5aeb41b50267c007921b
mike-petrov <mis5116@yandex.ru>
2019-11-07 13:12:20 +0300

    update page Projects
    Co-Authored-By: Леонид <leonid20.0831@gmail.com>

--- frontend/src/Containers/Home.jsx
+++ frontend/src/Containers/Home.jsx
@@ -1,56 +1,21 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
-// import Button from '../Components/UI/Button/Button.jsx';
-// import Input from '../Components/UI/Input/Input.jsx';
-import Loader from '../Components/UI/Loader/Loader.jsx';
-import CardsList from '../Components/CardsList/CardsList.jsx';
-
-import { getProjects, rateProject } from '../Functions/api';
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			projects: [],
+			// sample: [],
 		};
-		this.onRateProject = this.onRateProject.bind(this);
-	}
-
-	componentWillMount() {
-		getProjects().then((res) => {
-			this.setState({ projects: res });
-        });
-	}
-
-	onRateProject(projectId, _type) {
-		const arrayOutput = {
-			id: projectId,
-			type: _type,
-		};
-		rateProject(arrayOutput).then((res) => {
-			this.setState({ projects: res });
-		});
+		// this.handler = this.handler.bind(this);
 	}
 
 	render() {
-		const { projects } = this.state;
+		// const { sample } = this.state;
 		return (
 			<div className="content">
 				<div className="title">Главная</div>
-				{ /* <Button onClick={this.handler}>
-					Add projects in database
-				</Button>
-				<Input
-					className="error"
-				/> */}
-				{projects.length !== 0 ? (
-					<CardsList
-						projects={projects}
-						onRateProject={this.onRateProject}
-					/>
-				) : (
-					<Loader />
-				)}
 			</div>
 		);
 	}
--- frontend/src/Containers/Projects.jsx
+++ frontend/src/Containers/Projects.jsx
@@ -1,21 +1,155 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
+import Input from '../Components/UI/Input/Input.jsx';
+import Loader from '../Components/UI/Loader/Loader.jsx';
+import CardsList from '../Components/CardsList/CardsList.jsx';
+
+import { getProjects, rateProject, createProject } from '../Functions/api';
 
 class Projects extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			// sample: [],
+			activePanel: 'projects',
+			projects: [],
+			arrayProject: {
+				name: '',
+				description: '',
+				category: '',
+				status: '',
+				linkGit: '',
+			},
 		};
-		// this.handler = this.handler.bind(this);
+		this.onRateProject = this.onRateProject.bind(this);
+		this.onCreate = this.onCreate.bind(this);
+	}
+
+	componentWillMount() {
+		getProjects().then((res) => {
+			this.setState({ projects: res });
+        });
+	}
+
+	onRateProject(projectId, _type) {
+		const arrayOutput = {
+			id: projectId,
+			type: _type,
+		};
+		rateProject(arrayOutput).then((res) => {
+			this.setState({ projects: res });
+		});
+	}
+
+	onChanePanel(_panel) {
+		this.setState({ activePanel: _panel });
+	}
+
+	onCreate(_event, project) {
+		const { onPopup } = this.props;
+		createProject(project).then(() => {
+			// console.log(res);
+			onPopup(true, 'success');
+			getProjects().then((res) => {
+				this.setState({ projects: res });
+			});
+			this.onChanePanel('projects');
+		});
+		_event.preventDefault();
+	}
+
+	handleCreateProject(_e, _type) {
+		const { arrayProject } = this.state;
+		if (_type === 'name') {
+			this.setState({ arrayProject: { ...arrayProject, name: _e.target.value } });
+		} else if (_type === 'description') {
+			this.setState({ arrayProject: { ...arrayProject, description: _e.target.value } });
+		} else if (_type === 'category') {
+			this.setState({ arrayProject: { ...arrayProject, category: _e.target.value } });
+		} else if (_type === 'status') {
+			this.setState({ arrayProject: { ...arrayProject, status: _e.target.value } });
+		} else if (_type === 'linkGit') {
+			this.setState({ arrayProject: { ...arrayProject, linkGit: _e.target.value } });
+		}
 	}
 
 	render() {
-		// const { sample } = this.state;
+		const { projects, activePanel, arrayProject } = this.state;
 		return (
 			<div className="content">
-				<div className="title">Проекты</div>
+				{activePanel === 'projects' ? (
+					<>
+						<div className="title title_group">
+							<span>Проекты</span>
+							<div onClick={() => { this.onChanePanel('create'); }}>
+								<i className="fas fa-plus-circle" />
+							</div>
+						</div>
+						{projects.length !== 0 ? (
+							<CardsList
+								projects={projects}
+								onRateProject={this.onRateProject}
+							/>
+						) : (
+							<Loader />
+						)}
+					</>
+				) : (
+					<>
+						<div className="title title_group">
+							<span>Создать</span>
+							<div onClick={() => { this.onChanePanel('projects'); }}>
+								<i className="fas fa-times-circle" />
+							</div>
+						</div>
+						<form id="create" className="form_create" onSubmit={(_event) => { this.onCreate(_event, arrayProject); }}>
+							<Input
+								name="name"
+								type="text"
+								placeholder="Название"
+								value={arrayProject.name}
+								onChange={(_e) => { this.handleCreateProject(_e, 'name'); }}
+								required
+							/>
+							<Input
+								name="description"
+								type="text"
+								placeholder="Описание"
+								value={arrayProject.description}
+								onChange={(_e) => { this.handleCreateProject(_e, 'description'); }}
+								required
+							/>
+							<Input
+								name="category"
+								type="text"
+								placeholder="Категория"
+								value={arrayProject.category}
+								onChange={(_e) => { this.handleCreateProject(_e, 'category'); }}
+								required
+							/>
+							<Input
+								name="status"
+								type="text"
+								placeholder="Статус"
+								value={arrayProject.status}
+								onChange={(_e) => { this.handleCreateProject(_e, 'status'); }}
+								required
+							/>
+							<Input
+								name="linkGit"
+								type="text"
+								placeholder="GitHub (ссылка)"
+								value={arrayProject.linkGit}
+								onChange={(_e) => { this.handleCreateProject(_e, 'linkGit'); }}
+								required
+							/>
+							<Input
+								className="btn"
+								type="submit"
+								value="Создать"
+							/>
+						</form>
+					</>
+				)}
 			</div>
 		);
 	}
